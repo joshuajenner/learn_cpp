90e2c3b434098a3031cb3ddcf7465c926ee7b9b9
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2024-03-21 15:01:23 +0800

    Explicit

--- Explicit.cpp
+++ Explicit.cpp
@@ -8,7 +8,7 @@ private:
     int m_Age;
     float m_Num;
 public:
-    Entity(const std::string& name) 
+    Entity(const std::string& name)
         : m_Name(name), m_Age(-1), m_Num(0.0f) {}
     Entity(int age)
         : m_Name("Unkown"), m_Age(age), m_Num(0.0f) {}
@@ -19,21 +19,21 @@ public:
 };
 
 
-int main()
-{
-    // This code is clearer though
-    Entity a("Charno");
-    Entity b(22);
-
-    // You can implicitly cast a type to class like this
-    // Must have constructor with just that variable type
-    Entity c = 22;
-
-    // Since there is explicit, this won't work
-    Entity d = 0.1f;
-
-    std::cin.get();
-}
+//int main()
+//{
+//    // This code is clearer though
+//    Entity a("Charno");
+//    Entity b(22);
+//
+//    // You can implicitly cast a type to class like this
+//    // Must have constructor with just that variable type
+//    Entity c = 22;
+//
+//    // Since there is explicit, this won't work
+//    Entity d = 0.1f;
+//
+//    std::cin.get();
+//}
 
 
 
