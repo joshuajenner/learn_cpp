d24c190b5d1c340a522ae1662e69569c47ad1954
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2024-03-22 16:49:49 +0800

    Update CopyConstructor.cpp

--- src/CopyConstructor.cpp
+++ src/CopyConstructor.cpp
@@ -15,10 +15,39 @@ public:
         // Need to add a null termination char
         m_Buffer[m_Size] = 0;
     }
+    // Don't allow copying
+    // String(const String& other) = delete;
+
+    // Copy constructor
+    String(const String& other)
+        :m_Size(other.m_Size)
+    {
+        m_Buffer = new char[m_Size + 1];
+        memcpy(m_Buffer, other.m_Buffer, m_Size + 1);
+    }
+
+    ~String() 
+    {
+        delete[] m_Buffer;
+    }
+
+    char& operator[] (unsigned int index) 
+    {
+        return m_Buffer[index];
+    }
+
     // Declaring the operator as a friend
     // Non-member functions that need access to private class members need to be friends
     friend std::ostream& operator<<(std::ostream& stream, const String& string);
 };
+
+// should be marked as const since we aren't editing the argument
+void PrintString(const String& string) 
+{
+    // Always pass objects by const reference
+    // Should pass in a refernce so that the copy constructor isn't called
+    std::cout << string << std::endl;
+} 
 
 std::ostream& operator<<(std::ostream& stream, const String& string) 
 {
@@ -29,11 +58,13 @@ std::ostream& operator<<(std::ostream& stream, const String& string)
 int main()
 {
     String string = "Cherno";
+    String second = string;
+
+    second[2] = 'a';
 
     std::cout << string << std::endl;
+    std::cout << second << std::endl;
     
-
-
     std::cin.get();
 }
 
